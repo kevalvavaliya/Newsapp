cbe34d4e49981ee9553173b3937b33abaa02b3ef
keval vavaliya <kevalvavaliya@gmail.com>
2022-01-13 01:01:30 +0530

    Added React Bootstrap and updated Components to Bootstrap components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,41 @@
 import React from "react"
+import { Card,Button,Container,Row } from 'react-bootstrap';
 import { graphql } from "gatsby"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-const ProductComponent = ({ data }) => {
-  const {
-    allContentfulNews: { nodes: newsArticle },
-  } = data
-  console.log(newsArticle)
+
+const NewsComponent = ({ data }) => {
+  // destructuring  the nodes array and store it in variable
+  const { allContentfulNews: { nodes: newsArticle },} = data
   return (
     <>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"></link>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <div className="container my-3">
+    <Container className="my-3">
       <h2 className='text-center' style={{margintop:'90px'}}>Top Headlines</h2>
-      <div className="container my-5">
-      <div className="row ">
-        {newsArticle.map(product => {
-          return (
-            <div className="col md-4">
-            <div className="card" style={{width:"18rem"}}>
-              <img src={product.newsImage.file.url} className="card-img-top" alt={product.newsImage.file.fileName} />
-              <div className="card-body">
-                <h5 className="card-title">{product.newsTitle}</h5>
-                <p className="card-text">{product.newsDescription}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-            </div>
-          )
-        })}
-        </div>
-      </div>
-      </div>
+      <Container className="my-5">
+        <Row>
+          {newsArticle.map(news => {
+            return (
+              <Card style={{ width: '18rem'}} className="mx-3">
+                <Card.Img variant="top" src={news.newsImage.file.url} />
+                <Card.Body>
+                  <Card.Title>{news.newsTitle}</Card.Title>
+                  <Card.Text>
+                    {news.newsDescription}
+                  </Card.Text>
+                  <Button variant="primary">Go somewhere</Button>
+                </Card.Body>
+              </Card>
+            )
+          })}
+
+        </Row>
+      </Container>
+    </Container>
     </>
   )
 }
 
+// QUERY NEWS FORM CONTENTFUL API
 export const query = graphql`
   {
     allContentfulNews {
@@ -58,4 +59,4 @@ export const query = graphql`
   }
 `
 
-export default ProductComponent
+export default NewsComponent
